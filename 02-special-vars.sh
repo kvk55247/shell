fba0a25219325ff21e5d5a745c47dadf5ae7b117
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-25 08:15:30 +0530

    spaecial vars

--- 02-special-vars.sh
+++ 02-special-vars.sh
@@ -1,3 +1,11 @@
 #!/bin bash
 
 echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "user running script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
