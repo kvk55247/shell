140044fbcbb1d910b045e003a40cc6ce4a070a3d
kvk55247 <kurapothulavamsi@gmail.com>
2025-07-25 16:43:37 +0530

    spaecial vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -19,3 +19,19 @@ else
    echo "Installing mysql is ...Failure"
    exit 1
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+   echo "MYSQL is not installed ... going to install it"
+   dnf install mysql -y
+   if [ $? -eq 0 ]
+    echo "Installing mysql is ... success"
+else
+   echo "Installing mysql is ...Failure"
+   exit 1
+fi
+else
+    echo "MYSQL is already installed ... nothing to do"
+fi
